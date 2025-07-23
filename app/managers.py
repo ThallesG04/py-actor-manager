52f526e8fe0349c03807925893e6dcd1f4804641
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-07-23 17:16:18 -0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,55 @@
 import sqlite3
 
+from django.template.defaultfilters import first
+
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+
+        self.connection = sqlite3.connect(db_name)
+        self.connection.row_factory = sqlite3.Row
+
+    def create(self, first_name: str, last_name: str) -> None:
+        cursor = self.connection.cursor()
+        sql = f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)"
+        cursor.execute(sql, (first_name, last_name))
+        self.connection.commit()
+        cursor.close()
+
+    def all(self) -> None:
+        cursor = self.connection.cursor()
+        sql = f"SELECT * FROM {self.table_name}"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        cursor.close()
+
+        actors = []
+        for row in rows:
+            actor = Actor(
+                id=row["id"],
+                first_name=row["first_name"],
+                last_name=row["last_name"]
+            )
+
+            actors.append(actor)
+
+        return actors
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        cursor = self.connection.cursor()
+        sql = f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?"
+        cursor.execute(sql, (new_first_name, new_last_name, pk))
+        self.connection.commit()
+        cursor.close()
+
+    def delete(self, pk: int) -> None:
+        cursor = self.connection.cursor()
+        sql = f"DELETE FROM {self.table_name} WHERE id = ?"
+        cursor.execute(sql, (pk,))
+        self.connection.commit()
+        cursor.close()
--- app/models.py
+++ app/models.py
@@ -1,4 +1,11 @@
 from dataclasses import dataclass
 
 
-# add dataclass here
+class Actor(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_lenght=100)
+
+    def str(self) -> str:
+        return f"{self.first_name} {self.last_name}"
