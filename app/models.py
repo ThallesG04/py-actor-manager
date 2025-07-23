9629545473c22577669be22303ba07a8a53f5662
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-07-23 17:20:59 -0300

    New solution

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Actor(models.Model):
 
     id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_lenght=100)
+    last_name = models.CharField(max_length=100)
 
     def str(self) -> str:
         return f"{self.first_name} {self.last_name}"
